db7e3fbf49f642ab8e67b49cf31aad41a4a38c81
John Stevenson <john@jr0cket.co.uk>
2019-11-27 00:29:22 +0000

    New: Leiningen project
    Created a new leiningen project using the command
    
    lein new todo-list
    
    Updated the project.clj file with:
    * a project description
    * URL for the code repository
    * creative commons license
    * latest version of Clojure

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject todo-list "0.1.0-SNAPSHOT"
+
+  :description "A Todo List server-side webapp using Ring & Compojure"
+  :url "https://github.com/practicalli/clojure-todo-list-example"
+
+  :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
+            :url  "https://creativecommons.org"}
+
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+
+  :repl-options {:init-ns todo-list.core})
---
+++ src/todo_list/core.clj
@@ -0,0 +1,6 @@
+(ns todo-list.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
