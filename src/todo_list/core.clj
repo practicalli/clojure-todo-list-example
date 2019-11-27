8f1f52573621e55b0594dc8515886acbdb2ce3c3
John Stevenson <john@jr0cket.co.uk>
2019-11-27 04:25:32 +0000

    Checking for different URLs as part of the request
    If a request is something other than the root of the website, then return a
    message saying the page does not exist.

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,14 +1,21 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as webserver]))
 
+
 (defn welcome
-  "A ring handler to process all requests sent to the webapp"
+  "A ring handler to process all requests for the web server.
+  If a request is for something other than `/` then an error message is returned"
   [request]
-  {:status  200
-   :headers {}
-   :body    "<h1>Hello, Clojure World</h1>
-             <p>Welcome to your first Clojure app.
-             This message is returned regardless of the request, sorry<p>"})
+  (if (= "/" (:uri request))
+    {:status  200
+     :headers {}
+     :body    "<h1>Hello, Clojure World</h1>
+               <p>Welcome to your first Clojure app.</p>"}
+    {:status  404
+     :headers {}
+     :body    "<h1>This is not the page you are looking for</h1>
+               <p>Sorry, the page you requested was not found!</p>"}))
+
 
 (defn -main
   "A very simple web server using Ring & Jetty"
