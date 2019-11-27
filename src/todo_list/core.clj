fca10993566b249c55f00316bb4508f6faf63e34
John Stevenson <john@jr0cket.co.uk>
2019-11-27 04:15:56 +0000

    Added a welcome handler
    Created a separate function to take all requests and return a response map.
    This function is referred to as a handler in ring terminology.
    
    This simplifies the -main function

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,16 +1,19 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as webserver]))
 
+(defn welcome
+  "A ring handler to process all requests sent to the webapp"
+  [request]
+  {:status  200
+   :headers {}
+   :body    "<h1>Hello, Clojure World</h1>
+             <p>Welcome to your first Clojure app.
+             This message is returned regardless of the request, sorry<p>"})
 
 (defn -main
   "A very simple web server using Ring & Jetty"
   [port-number]
   (webserver/run-jetty
-    (fn [request]
-      {:status  200
-       :headers {}
-       :body    "<h1>Hello, Clojure World</h1>
-                 <p>Welcome to your first Clojure app.
-                 This message is returned regardless of the request, sorry</p>"})
+    welcome
     {:port  (Integer. port-number)
      :join? false}))
